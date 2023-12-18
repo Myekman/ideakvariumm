f1f45a46be093385d5ff92dccfab64a64665c590
Myekman <my@traningstemplet.se>
2023-12-18 13:10:42 +0000

    add post view for fishes

--- fishes/views.py
+++ fishes/views.py
@@ -1,13 +1,17 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Fish
 from .serializers import FishSerializer
 
 class FishList(generics.ListCreateAPIView):
     queryset = Fish.objects.all()
     serializer_class = FishSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        serializer.save
+    # def perform_create(self, serializer):
+    #     serializer.save(user=self.request.user)
+
+   
 
